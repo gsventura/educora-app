940118460a5c5442e685cd816a98a5b044982df6
gsventura <128729939+gsventura@users.noreply.github.com>
2024-05-15 21:37:58 -0300

    denovo

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
-export const Home = () => {
+const Home = () => {
   return (
     <div>
-      <p>Home</p>
+      
     </div>
   )
 }
 
 export default Home
+
