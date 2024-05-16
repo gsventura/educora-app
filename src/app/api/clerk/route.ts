a354cef73c26e9607211341c0fb487025bf45561
gsventura <128729939+gsventura@users.noreply.github.com>
2024-05-16 11:05:21 -0300

    nagain-webhook

--- src/app/api/clerk/route.ts
+++ src/app/api/clerk/route.ts
@@ -25,19 +25,19 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.json();
+  const payload = await req.json()
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(NEXT_CLERK_WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+  let evt: WebhookEvent
 
   // Verify the payload with the headers
   try {
@@ -45,12 +45,12 @@ export async function POST(req: Request) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    }) as WebhookEvent
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
   // Get the ID and type
